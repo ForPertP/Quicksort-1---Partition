24536428310cb9c9b036472373b309f941d8a9da
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-12-06 00:16:42 +0900

    Update Quicksort-1---Partition.cpp

--- Quicksort-1---Partition.cpp
+++ Quicksort-1---Partition.cpp
@@ -24,3 +24,44 @@ void median_sort(T& arr, int begin, int mid, int end)
         swap_value(arr[begin], arr[mid]);
     }
 }
+
+template <typename T>
+void quick_sort(T& arr, int begin, int end)
+{
+    int mid = begin + (end - begin) / 2;
+    median_sort(arr, begin, mid, end);
+
+    if (end - begin + 1 > 3)
+    {
+        int pivot = arr[mid];
+        swap_value(arr[mid], arr[end - 1]);
+
+        int i = begin;
+        int j = end - 1;
+
+        while (true)
+        {
+            while (arr[++i] < pivot && i < end);
+            while (arr[--j] > pivot && j > begin);
+
+            if (i >= j)
+            {
+                break;
+            }
+
+            swap_value(arr[i], arr[j]);
+        }
+        swap_value(arr[i], arr[end - 1]);
+
+        quick_sort(arr, begin, i - 1);
+        quick_sort(arr, i + 1, end);
+    }
+}
+
+vector<int> quickSort(vector<int> arr)
+{
+    int index = std::distance(arr.begin(), arr.end());
+    quick_sort(arr, 0, index - 1);
+
+    return arr;    
+}
