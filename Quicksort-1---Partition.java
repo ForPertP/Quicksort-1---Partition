f5c4d1b5d2875e892119dcf17369b1a5e53af3a2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-30 08:11:42 +0900

    Update Quicksort-1---Partition.java

--- Quicksort-1---Partition.java
+++ Quicksort-1---Partition.java
@@ -76,3 +76,29 @@ class Result {
         }
     }
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.quickSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
